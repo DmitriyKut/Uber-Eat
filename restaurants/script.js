e1116ad293fc0be8e33dcf5f36218ff58187a68f
DmitriyKut <dmitriykut00@yandex.ru>
2023-08-06 16:53:36 +0300

    Added Menu Management

--- restaurants/script.js
+++ restaurants/script.js
@@ -1,5 +1,39 @@
+//////////////////////////////////////////////Mobile menu//////////////////////////////////////////////////////////////////////////////
 $(document).ready(function() {
     $('.nav1').click(function(event) {
-        $('.nav4').toggleClass('.true');
+        $('.nav1,.nav2,.nav3,.nav4,.nav5,.nav6').toggleClass('active');
     });
 });
+
+$(document).ready(function() {
+    $('.nav2').click(function(event) {
+        $('.nav1,.nav3,.nav4,.nav5,.nav6').toggleClass('hide');
+    });
+});
+
+$(document).ready(function() {
+    $('.nav3').click(function(event) {
+        $('.nav1,.nav2,.nav4,.nav5,.nav6').toggleClass('hide');
+    });
+});
+
+$(document).ready(function() {
+    $('.nav4').click(function(event) {
+        $('.nav1,.nav2,.nav3,.nav5,.nav6').toggleClass('hide');
+    });
+});
+
+$(document).ready(function() {
+    $('.nav5').click(function(event) {
+        $('.nav1,.nav2,.nav3,.nav4,.nav6').toggleClass('hide');
+    });
+});
+
+$(document).ready(function() {
+    $('.nav6').click(function(event) {
+        $('.nav1,.nav2,.nav3,.nav4,.nav5').toggleClass('hide');
+    });
+});
+/////////////////////////////////////////////////Main menu///////////////////////////////////////////////////////////////////////////
+
+
