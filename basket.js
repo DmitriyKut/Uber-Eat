0c6a3b51c340a6ae87a7ae6559f89b44d01d3e81
DmitriyKut <dmitriykut00@yandex.ru>
2023-09-04 13:57:57 +0300

    Products Now Summed

--- basket.js
+++ basket.js
@@ -1,4 +1,10 @@
-let cartItemHTML;
+let cartItemHTML = new Array();
+let infoId = new Array();
+let infoCounter = new Array();
+let ch_for = 0;
+let ch_enter = 0;
+let repeat = -1;
+const wrapper = document.querySelector('.card_content');
 
 window.addEventListener('click', function(event) {
 
@@ -16,24 +22,64 @@ window.addEventListener('click', function(event) {
             price: card.querySelector('.price').innerText,
             counter: card.querySelector('[data-counter]').innerText,
         }
+        //Цикл запоминает id заказанных товаров и их колличество, а у повторяющихся товаров складывается counter и товар не дублируется
+        infoId[ch_for] = productInfo.id;
+        infoCounter[ch_for] = productInfo.counter;
+        ch_for = ch_for + 1;
 
-        //Собранные данные подставлены в шаблон корзины
-        cartItemHTML = cartItemHTML + `
-        <div class="card_content" data-id="${productInfo.id}">
-          <div class="card_info">
-            <div class="food">${productInfo.title}</div>
-            <div class="price"><b>${productInfo.price}</b></div>
-            <div class="items_counter-wrapper">
-              <div class="items_control" data-action="minus">&#8722;</div>
-              <div class="items_current" data-counter>${productInfo.counter}</div>
-              <div class="items_control" data-action="plus">+</div>
+        for (let i = 0; i < ch_for ; i++) {
+          for (let j = i + 1; j < ch_for; j++) {
+            if (infoId[i] === infoId[j]) {
+              infoCounter[i] = Number(infoCounter[i]) + Number(infoCounter[j]);
+              infoId.length = ch_for - 1;
+              infoCounter.length = ch_for - 1;
+              ch_for--;
+              //repeat = порядку товара, который заказан повторно
+              repeat = i;
+            }
+          }
+        }
+
+        if (repeat === -1 && productInfo.counter != 0) {
+          //Собранные данные подставлены в шаблон корзины
+          cartItemHTML[ch_enter] = 
+          `<div class="card_content" data-id="${productInfo.id}">
+            <div class="card_info">
+              <div class="food">${productInfo.title}</div>
+              <div class="price"><b>${productInfo.price}</b></div>
+              <div class="items_counter-wrapper">
+                <div class="items_control" data-action="minus">&#8722;</div>
+                <div class="items_current" data-counter>${productInfo.counter}</div>
+                <div class="items_control" data-action="plus">+</div>
+              </div>
             </div>
-          </div>
-        <img src="../restaurants/${productInfo.imgSrc}" width="170" height="160" class="picture"/>
-        </div>`;
+          <img src="../restaurants/${productInfo.imgSrc}" width="170" height="160" class="picture"/>
+          </div>`;
+          ch_enter++;
+        } else {
+          //Заменяем counter товара, котоый заказан повторно
+          cartItemHTML[repeat] = 
+          `<div class="card_content" data-id="${productInfo.id}">
+            <div class="card_info">
+              <div class="food">${productInfo.title}</div>
+              <div class="price"><b>${productInfo.price}</b></div>
+              <div class="items_counter-wrapper">
+                <div class="items_control" data-action="minus">&#8722;</div>
+                <div class="items_current" data-counter>${infoCounter[repeat]}</div>
+                <div class="items_control" data-action="plus">+</div>
+              </div>
+            </div>
+          <img src="../restaurants/${productInfo.imgSrc}" width="170" height="160" class="picture"/>
+          </div>`;
+          repeat = -1;
+        }
+
+        console.log(cartItemHTML);
+
+        //Создаем хранилище с данными карточек товаров, добавленных в корзину
         localStorage.setItem('want', cartItemHTML);
     }
-
+  // Передаем хранилище на страницу с корзиной
   if (event.target.hasAttribute('data-shop')) {
     document.location.href = '../basket/index.html';
     }
--- basket/transfer.js
+++ basket/transfer.js
@@ -1,2 +1,5 @@
+//Ищем шаблон карточки
 const cartWrapper = document.querySelector('.card_content');
+
+//Добавляем карточки со страницы магазина после шаблона
 cartWrapper.insertAdjacentHTML('AfterEnd', localStorage.getItem('want'));
