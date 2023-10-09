1335f3722875556d6a935f15ece66490d43ec5f4
DmitriyKut <dmitriykut00@yandex.ru>
2023-10-09 16:05:32 +0300

    Release Bugs Fixed

--- restaurants/counter.js
+++ restaurants/counter.js
@@ -22,3 +22,28 @@ window.addEventListener('touchstart', function (event) {            //touchstart
         }
     }
 });
+
+// Добавление поиска на всем окне
+window.addEventListener('click', function (event) {            //click - событие клика (для ПК)
+
+    let counter;
+
+    // Касание было по кнопке "+" или "-"?
+    if (event.target.dataset.action === 'plus' || event.target.dataset.action === 'minus') {
+        
+        // Поиск обертки счетчика
+        const counterWrapper = event.target.closest('.items_counter-wrapper');
+
+        // Поиск счетчика внутри обертки
+        counter = counterWrapper.querySelector('[data-counter]');
+    }
+
+    if (event.target.dataset.action === 'plus') {
+        counter.innerText = ++counter.innerText;
+    }
+    if (event.target.dataset.action === 'minus') {
+        if (parseInt(counter.innerText) > 0) {
+            counter.innerText = --counter.innerText;
+        }
+    }
+});
